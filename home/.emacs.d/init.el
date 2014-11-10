a89152403dfabc25bfa89e759c4672b4fc0eb350
Simon Ye <sye737@gmail.com>
2014-11-10 13:14:07 -0800

    Comment out emacs shell /usr/local/bin /usr/local/sbin /Users/simon/.pyenv/bin /Users/simon/bin /Users/simon/.pyenv/shims /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /usr/texbin to get jedi working.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,11 +1,11 @@
 ;; Load user $PATH
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell
-         (replace-regexp-in-string "[[:space:]\n]*$" ""
-                                   (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-(when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
+;; (defun set-exec-path-from-shell-PATH ()
+;;   (let ((path-from-shell
+;;          (replace-regexp-in-string "[[:space:]\n]*$" ""
+;;                                    (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+;;     (setenv "PATH" path-from-shell)
+;;     (setq exec-path (split-string path-from-shell path-separator))))
+;; (when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
 
 (require 'package)
 (add-to-list 'package-archives
