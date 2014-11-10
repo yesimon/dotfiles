8de9248f72b472c1300bc05300ec9e2c36950dbe
Simon Ye <sye737@gmail.com>
2014-11-10 11:49:14 -0800

    Updates to fish config for pyenv, pip cache, go.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,14 +1,23 @@
-set -x PATH "$HOME/.pyenv/bin" $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
+if test -d "$HOME/.pyenv"
+ set -x PATH "$HOME/.pyenv/bin" $PATH
+ status --is-interactive; and . (pyenv init -|psub)
+ status --is-interactive; and . (pyenv virtualenv-init -|psub)
+end
+
+if test -d "$HOME/go"
+   set -x GOPATH "$HOME/go"
+   set -x fish_user_paths "/usr/local/go/bin" "$HOME/go/bin" $fish_user_paths
+end
 
 set -x fish_user_paths "$HOME/bin" $fish_user_paths
-set -x fish_user_paths "/usr/local/go/bin" "$HOME/go/bin" $fish_user_paths
-set -x GOPATH "$HOME/go"
-
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 
 source "$HOME/bin/virtual.fish"
+
+set -x PIP_USE_WHEEL "true"
+set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
+set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
+set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 
 if test -e "$HOME/.config/fish/local.fish"
   source "$HOME/.config/fish/local.fish"
