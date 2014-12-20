b754376a15b5f7ee1c95383f0ea651937320bef1
Simon Ye <sye737@gmail.com>
2014-12-20 00:17:23 -0500

    Update emacs init.
    No more flymake.
    No more ack-and-a-half.
    No more coffee-custom.
    Update for new coffee mode.
    Use exec-path-from-shell.
    Use ag.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,15 +1,6 @@
-;; Load user $PATH
-;; (defun set-exec-path-from-shell-PATH ()
-;;   (let ((path-from-shell
-;;          (replace-regexp-in-string "[[:space:]\n]*$" ""
-;;                                    (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
-;;     (setenv "PATH" path-from-shell)
-;;     (setq exec-path (split-string path-from-shell path-separator))))
-;; (when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
-
 (require 'package)
 (add-to-list 'package-archives
-       '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
@@ -27,17 +18,14 @@
       (eval-print-last-sexp))))
 
 (setq el-get-sources
-      '((:name smex
+      '((:name exec-path-from-shell
+               :after (when (memq window-system '(mac ns))
+                        (exec-path-from-shell-initialize)))
+        (:name smex
                :after (progn (global-set-key (kbd "M-x") 'smex)
                              (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
         (:name flycheck
                :after (add-hook 'after-init-hook #'global-flycheck-mode))
-        (:name flymake-python-pyflakes
-               :type github
-               :pkgname "purcell/flymake-python-pyflakes"
-               :depends (flymake-easy)
-               :post-init (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-               :features flymake-python-pyflakes)
         (:name thrift
                :type http
                :url "https://raw.github.com/apache/thrift/master/contrib/thrift.el"
@@ -49,8 +37,6 @@
                :type http
                :url "https://raw.githubusercontent.com/JuliaLang/julia/master/contrib/julia-mode.el"
                :features julia-mode)
-        (:name flymake-cursor
-               :features flymake-cursor)
         (:name projectile
                :after (projectile-global-mode)
                :features projectile)
@@ -58,13 +44,18 @@
                :type github
                :pkgname "m00natic/anti-zenburn-theme"
                :prepare (add-to-list 'custom-theme-load-path default-directory))
+        (:name color-theme-zenburn
+               :after (add-to-list 'safe-local-eval-forms
+                                   '(when (require 'rainbow-mode nil t) (rainbow-mode 1))))
         (:name color-theme-base16
                :type github
                :pkgname "milkypostman/base16-emacs"
                :prepare (add-to-list 'custom-theme-load-path default-directory))
         (:name coffee-mode
                :type elpa
-               :after (load "~/.emacs.d/coffee-custom"))
+               :after  (progn
+                         (custom-set-variables '(coffee-tab-width 2))
+                         (add-hook 'coffee-mode-hook 'coffee-cos-mode)))
         (:name web-mode
                :description "emacs major mode for editing PHP/JSP/ASP HTML templates (with embedded CSS and JS blocks)"
                :type github
@@ -87,29 +78,28 @@
         ))
 
 (defvar my-el-get-packages
-      '(smex
-        pkg-info
-        ido-ubiquitous
-        idle-highlight-mode
-        magit
-        coffee-mode
-        yaml-mode
-        markdown-mode
-        flycheck
-        julia-mode
-        ;; go-mode
-        ;; flymake-python-pyflakes
-        ;; flymake-coffee
-        ;; flymake-cursor
-        projectile
-        jedi
-        thrift
-        ack-and-a-half
-        color-theme-zenburn
-        rainbow-mode
-        revive-plus
-        frame-restore
-        web-mode))
+  '(exec-path-from-shell
+    smex
+    pkg-info
+    ido-ubiquitous
+    idle-highlight-mode
+    magit
+    coffee-mode
+    yaml-mode
+    markdown-mode
+    flycheck
+    julia-mode
+    ;; go-mode
+    projectile
+    jedi
+    thrift
+    ack
+    ag
+    color-theme-zenburn
+    rainbow-mode
+    revive-plus
+    frame-restore
+    web-mode))
 
 (el-get-cleanup my-el-get-packages)
 (el-get 'sync my-el-get-packages)
@@ -161,15 +151,13 @@
 (load-theme 'zenburn t)
 
 (custom-set-faces
-   `(flycheck-error
-      ((t (:foreground ,(cdr (assoc "zenburn-red-1" zenburn-colors-alist))
-                      :weight bold :underline t))))
-   `(flycheck-warning
-      ((t (:foreground ,(cdr (assoc "zenburn-yellow" zenburn-colors-alist))
-                     :weight bold :underline t))))
-   `(flycheck-info
-      ((t (:foreground ,(cdr (assoc "zenburn-cyan" zenburn-colors-alist))
-                     :weight bold :underline t)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-error ((t (:foreground "#BC8383" :weight bold :underline t))))
+ '(flycheck-info ((t (:foreground "#93E0E3" :weight bold :underline t))))
+ '(flycheck-warning ((t (:foreground "#F0DFAF" :weight bold :underline t)))))
 
 (load "~/.emacs.d/misc.el" t)
 (load "~/.emacs.d/local.el" t)
