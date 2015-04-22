e7177e9e0c8bc5dd5ad6f69b0178233408be6d9f
Simon Ye <sye737@gmail.com>
2015-04-21 22:56:02 -0400

    Reindent fish and add LESSPIPE env var.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,25 +1,25 @@
 set -g -x PATH /usr/local/bin /usr/local/sbin $PATH
 switch (uname)
-  case Linux
-    set -x SHELL /usr/bin/fish
-  case Darwin
-    set -x SHELL /usr/local/bin/fish
+case Linux
+  set -x SHELL /usr/bin/fish
+case Darwin
+  set -x SHELL /usr/local/bin/fish
 end
 
 if test -d "$HOME/.pyenv"
- set -x PATH "$HOME/.pyenv/bin" $PATH
- status --is-interactive; and . (pyenv init -|psub)
- # status --is-interactive; and . (pyenv virtualenv-init -|psub)
+  set -x PATH "$HOME/.pyenv/bin" $PATH
+  status --is-interactive; and . (pyenv init -|psub)
+  # status --is-interactive; and . (pyenv virtualenv-init -|psub)
 end
 
 if test -d "$HOME/go"
-   set -x GOPATH "$HOME/go"
-   set -x fish_user_paths "/usr/local/go/bin" "$HOME/go/bin" $fish_user_paths
+  set -x GOPATH "$HOME/go"
+  set -x fish_user_paths "/usr/local/go/bin" "$HOME/go/bin" $fish_user_paths
 end
 
 # Set up perl's local::lib
 if test -d "$HOME/perl5"
-   eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
+  eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 end
 
 set -x fish_user_paths "$HOME/bin" $fish_user_paths
@@ -38,6 +38,8 @@ source "$HOME/bin/virtual.fish"
 set -x PIP_USE_WHEEL "true"
 set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
 set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
+
+set -x LESSOPEN "|/usr/local/bin/lesspipe.sh %s"
 
 if test -e "$HOME/.config/fish/local.fish"
   source "$HOME/.config/fish/local.fish"
@@ -66,22 +68,22 @@ function inc --description 'Increase the value of variable' --no-scope-shadowing
 end
 
 function varclear --description 'Remove duplicates from environment variable'
-    if test (count $argv) = 1
-        set -l newvar
-        set -l count 0
-        for v in $$argv
-            if contains -- $v $newvar
-                inc count
-            else
-                set newvar $newvar $v
-            end
-        end
-        set $argv $newvar
-    else
-        for a in $argv
-            varclear $a
-        end
-    end
+  if test (count $argv) = 1
+    set -l newvar
+    set -l count 0
+    for v in $$argv
+      if contains -- $v $newvar
+        inc count
+          else
+            set newvar $newvar $v
+          end
+      end
+      set $argv $newvar
+  else
+    for a in $argv
+      varclear $a
+      end
+  end
 end
 
 # Add this back when it works again.
