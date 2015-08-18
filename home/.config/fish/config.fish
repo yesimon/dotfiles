6cad767303ff7dfb68b2ef6f363995e11aeacbb0
Simon Ye <sye737@gmail.com>
2015-08-18 01:40:07 -0400

    Fixup LESSPIPE, fish ubuntu/terminator colors

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,16 +2,32 @@ set -g -x PATH /usr/local/bin /usr/local/sbin $PATH
 switch (uname)
 case Linux
   set -x SHELL /usr/bin/fish
+  set -x LESSOPEN "|/usr/bin/lesspipe %s"
 case Darwin
   set -x SHELL /usr/local/bin/fish
+  set -x LESSOPEN "|/usr/local/bin/lesspipe.sh %s"
 end
 
+# Python setup
 if test -d "$HOME/.pyenv"
   set -x PATH "$HOME/.pyenv/bin" $PATH
   status --is-interactive; and . (pyenv init -|psub)
   # status --is-interactive; and . (pyenv virtualenv-init -|psub)
 end
 
+if test -d "$HOME/.local/bin"
+  set -x fish_user_paths "$HOME/.local/bin" $fish_user_paths
+end
+
+if test -e "$HOME/bin/virtual.fish"
+  source "$HOME/bin/virtual.fish"
+end
+
+set -x PIP_USE_WHEEL "true"
+set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
+set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
+
+# Go lang setup
 if test -d "$HOME/go"
   set -x GOPATH "$HOME/go"
   set -x fish_user_paths "$HOME/go/bin" $fish_user_paths
@@ -22,6 +38,7 @@ if test -d "$HOME/go"
   end
 end
 
+# Perl setup
 # Set up perl's local::lib
 if test -d "$HOME/perl5"
   eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
@@ -33,18 +50,6 @@ source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 if test -d "$HOME/local/bin"
   set -x fish_user_paths "$HOME/local/bin" $fish_user_paths
 end
-
-if test -d "$HOME/.local/bin"
-  set -x fish_user_paths "$HOME/.local/bin" $fish_user_paths
-end
-
-source "$HOME/bin/virtual.fish"
-
-set -x PIP_USE_WHEEL "true"
-set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
-set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
-
-set -x LESSOPEN "|/usr/local/bin/lesspipe.sh %s"
 
 if test -e "$HOME/.config/fish/local.fish"
   source "$HOME/.config/fish/local.fish"
@@ -98,6 +103,36 @@ function varclear --description 'Remove duplicates from environment variable'
   end
 end
 
+function fish_load_colors --description 'Restore colors'
+  set fish_color_autosuggestion 777
+  set fish_color_command blue
+  set fish_color_comment red
+  set fish_color_cwd green
+  set fish_color_cwd_root red
+  set fish_color_end green
+  set fish_color_error red --bold
+  set fish_color_escape cyan
+  set fish_color_history_current cyan
+  set fish_color_host -o cyan
+  set fish_color_match cyan
+  set fish_color_normal normal
+  set fish_color_operator cyan
+  set fish_color_param yellow
+  set fish_color_quote yellow
+  set fish_color_redirection normal
+  set fish_color_search_match --background=111111
+  set fish_color_selection --background=purple
+  set fish_color_status red
+  set fish_color_user -o green
+
+  set fish_color_valid_path --underline
+  set fish_pager_color_completion white
+  set fish_pager_color_description white
+  set fish_pager_color_prefix blue
+  set fish_pager_color_progress cyan
+end
+
+fish_load_colors
 # Add this back when it works again.
 # set -x fish_user_paths $fish_user_paths /usr/local/bin
 varclear PATH
